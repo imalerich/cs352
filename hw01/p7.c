511ae072c731fbc9c55630fbcf2a049a349fb3a3
Ian Malerich <ian.malerich@gmail.com>
2017-09-02 19:57:44 -0500

    finished hw01

--- hw01/p7.c
+++ hw01/p7.c
@@ -4,12 +4,12 @@ int main() {
 	double y;
 	double * z;
 
-	/* (1) */ z = (double *)&w
+	/* (1) */ z = (double *)&w;
 	/* (2) */ scanf("%f", &y);
 	/* (3) */ *z = sqrt(w);
-	/* (4) */ y = y*2.0;
+	/* (4) */ y = y * 2.0;
 	/* (5) */ printf("y=%f, *z=%f\n", y, *z);
 	/* (6) */ y = y/x;
-	/* (7) */ w = y*z;
+	/* (7) */ w = y * z;
 	return 0;
 }
