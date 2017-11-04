1e550f6ef55e969b261f476f31ee2c69f863b9e7
Ian Malerich <ian.malerich@gmail.com>
2017-11-04 18:38:29 -0500

    each compiles and runs, but neither connect

--- pr02/client/main.c
+++ pr02/client/main.c
@@ -1,20 +1,56 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+
 #include <openssl/sha.h>
 #include <encoder.h>
 
 int main(int argc, char ** argv) {
-	printf("Enter a String to be Sent: ");
+	const int portno = 8675309;
+	const char * ip_addr = "127.0.0.1";
 
-	// read the data package we wish to send
+	/* -- Read the line to send. --- */
 	char * line = NULL;
 	size_t len  = 0;
+	printf("Enter a String to be Sent: ");
 	getline(&line, &len, stdin);
 
-	// calculate the 20-Byte SHA-1 hash
+	// TODO: Encrypt the line before sending to the server.
+
+	/* --- Calculate the 20-Byte SHA-1 hash. --- */
 	unsigned char hash[SHA_DIGEST_LENGTH];
 	SHA1(line, len, hash);
+
+	/* --- Setup the socket. --- */
+	int socketfd = socket(AF_INET, SOCK_STREAM, 0);
+
+	/* --- Connect to the server. --- */
+	struct hostent * server = gethostbyname(ip_addr);
+
+	struct sockaddr_in serv_addr;
+	bzero((char *)&serv_addr, sizeof(serv_addr));
+	serv_addr.sin_family = AF_INET;
+	serv_addr.sin_port = htons(portno);
+	bcopy((char *)server->h_addr, 
+		(char *)&serv_addr.sin_addr.s_addr, server->h_length);
+
+	connect(socketfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr));
+
+	/* --- Send message to server. --- */
+	write(socketfd, line, len);
+	write(socketfd, hash, SHA_DIGEST_LENGTH);
+
+	/* --- Output response from server. --- */
+	char buffer[256];
+	bzero(buffer, 256);
+	read(socketfd, buffer, 256);
+	printf("%s\n", buffer);
 
 	free(line);
 	return 0;
--- pr02/server/main.c
+++ pr02/server/main.c
@@ -1,8 +1,47 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
+
+#include <netdb.h>
+#include <netinet/in.h>
 
 int main(int argc, char ** argv) {
-	// TODO
+	const int portno = 8675309;
+
+	/* --- Setup the socket. --- */
+	int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+
+	/* --- Setup the server. --- */
+	struct sockaddr_in serv_addr;
+	bzero((char *)&serv_addr, sizeof(serv_addr));
+
+	serv_addr.sin_family = AF_INET;
+	serv_addr.sin_addr.s_addr = INADDR_ANY;
+	serv_addr.sin_port = htons(portno);
+
+	bind(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr));
+
+	listen(sockfd, 5);
+	struct sockaddr_in client_addr;
+
+	/* --- Connect to a client. --- */
+	int clilen = sizeof(client_addr);
+	int newsockfd = accept(sockfd, (struct sockaddr *)&client_addr, &clilen);
+
+	/* --- Begin communication. --- */
+	char buffer[256];
+	bzero(buffer, 256);
+	read(newsockfd, buffer, 255); 
+	// TODO: buffer is the line
+	printf("%s\n", buffer);
+	read(newsockfd, buffer, 255);
+	// TODO: buffer is the SHA-1 encoding
+	printf("%s\n", buffer);
+	
+	const char * msg = "Message";
+	write(newsockfd, msg, strlen(msg));
+	close(newsockfd);
+
 	return 0;
 }
